4c17fb78c06ccdcc9ac6ebc469038d6e787d6e58
deustch2acc <ammogu.s.1.2.3.4.5.6@gmail.com>
2026-04-15 13:48:53 +0000

    auto backup Wed Apr 15 13:48:53 UTC 2026

--- autobackup.sh
+++ autobackup.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-THRESHOLD=25
 
 while true
 do
-    CHANGED=$(git status --porcelain | wc -1)
-
-    if [ "$CHANGED" -ge
-"$THRESHOLD" ]; then
         git add .
         git commit -m "auto backup $(date)"
         git push
-    fi
 
-    sleep 600
+    sleep 1200
 
 done
